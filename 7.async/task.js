3096668de8cd5dd026e106340686c3e377dea47a
AngelinaVB <b.lina.1995@mail.ru>
2025-01-08 23:46:57 +0300

    задание частично выполнено

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,44 +1,43 @@
 
 class AlarmClock {
-    constructor (alarmCollection = [], intervalId) {
+    constructor (alarmCollection = [], intervalId = null) {
         this.alarmCollection = alarmCollection;
         this.intervalId = intervalId;
     };
 
-    addClock (time, func) {
-        if (time == 0 || func == 0) {
+    addClock (time, f) {
+        if (!time || !f) {
             throw new Error ('Отсутствуют обязательные аргументы');
         };
-        if (time == true) {
+        if (this.alarmCollection.some(f)) {
             console.warn('Уже присутствует звонок на это же время');
         };
 
         this.alarmCollection.push(
-            {callback: func, time: time, canCall: true}
-        )
+            {callback: f, time: time, canCall: true});
+    };
+   
+    removeClock () {
+        alarmCollection.filter((timeNew) => timeNew == time);
+        return timeNew;  
     };
 
-    removeClock (time) {
-      
-      /* alarmCollection.filter(time => time.time == true); */
-        
+    getCurrentFormattedTime () {
+        let date = new Date().toLocaleTimeString("ru", {timeStyle: "short"});
+        return date;
     };
 
-    getCurrentFormattedTime (time) {
-        this._time = time;
-        time = new Date().toLocaleTimeString().slice(0,-3);
-    };
-
-    start () {
-        if (intervalId == true) {
-            return;
-        }
-        this.alarmCollection.forEach(());
-        return;
+    start () { 
+       if (intervalId == null) {
+       this.alarmCollection.forEach((time) => time == date);
+       let intervalId = setInterval(f(),);
+       }
+       else {}
     };
     
     stop () {
-
+        clearInterval(interval);
+        this.intervalId = null;
     };
 
     resetAllCalls () {
@@ -46,16 +45,14 @@ class AlarmClock {
     };
 
     clearAlarms () {
-
+     stop();
+     this.alarmCollection = [];
     }
 };
 
-let clock;
-
-console.log(clock = new AlarmClock());
+const clock = new AlarmClock();
+const callback = f => f;
+clock.addClock("16:45", callback); 
 clock.addClock("16:45", callback);
-console.log(clock.alarmCollection[0].time);
-
-console.log(clock.getCurrentFormattedTime());
-
-clock.start();
+clock.addClock("16:46", callback); 
+clock.addClock("21:54", callback);
